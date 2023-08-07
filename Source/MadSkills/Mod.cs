25ecd692f537c98bf578b2c37ec40b43a50054a0
syrinka <syrinka@foxmail.com>
2023-08-07 16:19:58 +0800

    replace tab to space

--- Source/MadSkills/Mod.cs
+++ Source/MadSkills/Mod.cs
@@ -5,26 +5,26 @@ using Verse;
 
 namespace RTMadSkills
 {
-	public class Mod : Verse.Mod
-	{
-		public ModSettings settings;
+    public class Mod : Verse.Mod
+    {
+        public ModSettings settings;
 
-		public Mod(ModContentPack content) : base(content)
-		{
-			var harmony = new Harmony("io.github.ratysz.madskills");
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
+        public Mod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("io.github.ratysz.madskills");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-			settings = GetSettings<ModSettings>();
-		}
+            settings = GetSettings<ModSettings>();
+        }
 
-		public override string SettingsCategory()
-		{
-			return settings.SettingsCategory();
-		}
+        public override string SettingsCategory()
+        {
+            return settings.SettingsCategory();
+        }
 
-		public override void DoSettingsWindowContents(Rect inRect)
-		{
-			settings.DoSettingsWindowContents(inRect);
-		}
-	}
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            settings.DoSettingsWindowContents(inRect);
+        }
+    }
 }
--- Source/MadSkills/Patch_SkillRecord.cs
+++ Source/MadSkills/Patch_SkillRecord.cs
@@ -8,94 +8,100 @@ using Verse;
 
 namespace RTMadSkills
 {
-	[HarmonyPriority(Priority.HigherThanNormal)]
-	[HarmonyPatch(typeof(SkillRecord))]
-	[HarmonyPatch("Interval")]
-	internal static class Patch_SkillRecordInterval
-	{
-		// VSE compatible
-		private static bool VSE = ModLister.HasActiveModWithName("Vanilla Skills Expanded");
+    [HarmonyPriority(Priority.HigherThanNormal)]
+    [HarmonyPatch(typeof(SkillRecord))]
+    [HarmonyPatch("Interval")]
+    internal static class Patch_SkillRecordInterval
+    {
+        // VSE compatible
+        private static bool VSE = ModLister.HasActiveModWithName("Vanilla Skills Expanded");
         private static MethodInfo ForgetRateFactor = AccessTools.Method("VSE.Passions.PassionManager:ForgetRateFactor");
 
-		private static bool Prefix(SkillRecord __instance)
-		{
-			if (ModSettings.sleepStopDecaying && __instance.Pawn.Awake())
-			{
-				return false;
-			}
-			if (!ModSettings.tiered || __instance.XpProgressPercent > 0.1f)
-			{
-				float greatMemMultiplier = (ModSettings.greatMemoryAltered || !__instance.Pawn.story.traits.HasTrait(TraitDefOf.GreatMemory)) ? 1f : 0.5f;
-				float xpToLearn = greatMemMultiplier * VanillaMultiplier(__instance.levelInt) * ModSettings.multiplier;
-				if (VSE)
-				{
-					xpToLearn *= (float)ForgetRateFactor.Invoke(null, new object[] { __instance });
-				}
-				if (xpToLearn != 0.0f)
-				{
-					__instance.Learn(xpToLearn, false);
-				}
-			}
-			return false;
-		}
+        private static bool Prefix(SkillRecord __instance)
+        {
+            if (ModSettings.sleepStopDecaying && __instance.Pawn.Awake())
+            {
+                return false;
+            }
+            if (ModSettings.retentionHours > 0f
+                && Retention.tickRecord.TryGetValue(__instance.RetentionID(), out int tick)
+                && tick + ModSettings.retentionHours * 2500 > Find.TickManager.TicksGame)
+            {
+                return false;
+            }
+            if (!ModSettings.tiered || __instance.XpProgressPercent > 0.1f)
+            {
+                float greatMemMultiplier = (ModSettings.greatMemoryAltered || !__instance.Pawn.story.traits.HasTrait(TraitDefOf.GreatMemory)) ? 1f : 0.5f;
+                float xpToLearn = greatMemMultiplier * VanillaMultiplier(__instance.levelInt) * ModSettings.multiplier;
+                if (VSE)
+                {
+                    xpToLearn *= (float)ForgetRateFactor.Invoke(null, new object[] { __instance });
+                }
+                if (xpToLearn != 0.0f)
+                {
+                    __instance.Learn(xpToLearn, false);
+                }
+            }
+            return false;
+        }
 
-		private static float VanillaMultiplier(int level)
-		{
-			switch (level)
-			{
-				case 10: return -0.1f;
-				case 11: return -0.2f;
-				case 12: return -0.4f;
-				case 13: return -0.6f;
-				case 14: return -1.0f;
-				case 15: return -1.8f;
-				case 16: return -2.8f;
-				case 17: return -4.0f;
-				case 18: return -6.0f;
-				case 19: return -8.0f;
-				case 20: return -12.0f;
-				default: return 0.0f;
-			}
-		}
-	}
+        private static float VanillaMultiplier(int level)
+        {
+            switch (level)
+            {
+                case 10: return -0.1f;
+                case 11: return -0.2f;
+                case 12: return -0.4f;
+                case 13: return -0.6f;
+                case 14: return -1.0f;
+                case 15: return -1.8f;
+                case 16: return -2.8f;
+                case 17: return -4.0f;
+                case 18: return -6.0f;
+                case 19: return -8.0f;
+                case 20: return -12.0f;
+                default: return 0.0f;
+            }
+        }
+    }
 
-	[HarmonyPatch(typeof(SkillRecord))]
-	[HarmonyPatch("LearnRateFactor")]
-	internal static class Patch_SkillRecordLearnRateFactor
-	{
-		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-		{
-			var _instructions = instructions.MethodReplacer(
-				AccessTools.Property(typeof(SkillRecord), nameof(SkillRecord.LearningSaturatedToday)).GetGetMethod(),
-				AccessTools.Method(typeof(Patch_SkillRecordLearnRateFactor), nameof(Patch_SkillRecordLearnRateFactor.LearningSaturatedToday))
-			);
-			var patched = false;
-			foreach (var instruction in _instructions)
-			{
-				if (!patched && instruction.opcode == OpCodes.Ldc_R4 && System.Convert.ToSingle(instruction.operand) == 0.2f)
-				{
-					patched = true;
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(ModSettings), nameof(ModSettings.saturatedXPMultiplier)));
-					continue;
-				}
-				yield return instruction;
-			}
-		}
+    [HarmonyPatch(typeof(SkillRecord))]
+    [HarmonyPatch("LearnRateFactor")]
+    internal static class Patch_SkillRecordLearnRateFactor
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var _instructions = instructions.MethodReplacer(
+                AccessTools.Property(typeof(SkillRecord), nameof(SkillRecord.LearningSaturatedToday)).GetGetMethod(),
+                AccessTools.Method(typeof(Patch_SkillRecordLearnRateFactor), nameof(Patch_SkillRecordLearnRateFactor.LearningSaturatedToday))
+            );
+            var patched = false;
+            foreach (var instruction in _instructions)
+            {
+                if (!patched && instruction.opcode == OpCodes.Ldc_R4 && System.Convert.ToSingle(instruction.operand) == 0.2f)
+                {
+                    patched = true;
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(ModSettings), nameof(ModSettings.saturatedXPMultiplier)));
+                    continue;
+                }
+                yield return instruction;
+            }
+        }
 
-		private static bool LearningSaturatedToday(SkillRecord instance)
-		{
-			return instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
-		}
-	}
+        private static bool LearningSaturatedToday(SkillRecord instance)
+        {
+            return instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
+        }
+    }
 
-	[HarmonyPatch(typeof(SkillRecord))]
-	[HarmonyPatch("LearningSaturatedToday", MethodType.Getter)]
-	internal static class Patch_SkillRecordLearningSaturatedToday
-	{
-		private static bool Prefix(SkillRecord __instance, ref bool __result)
-		{
-			__result = __instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
-			return false;
-		}
-	}
+    [HarmonyPatch(typeof(SkillRecord))]
+    [HarmonyPatch("LearningSaturatedToday", MethodType.Getter)]
+    internal static class Patch_SkillRecordLearningSaturatedToday
+    {
+        private static bool Prefix(SkillRecord __instance, ref bool __result)
+        {
+            __result = __instance.xpSinceMidnight > ModSettings.dailyXPSaturationThreshold;
+            return false;
+        }
+    }
 }
